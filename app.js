847640ce4250abbbcf9cb490cd5572463f107b02
dani <danibarstad@gmail.com>
2021-06-09 22:17:58 -0500

    completed command line version of program

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const Twitter = require('twitter');
-
 const client = new Twitter({
     consumer_key: process.env.TWITTER_KEY,
     consumer_secret: process.env.TWITTER_SECRET,
@@ -9,12 +8,22 @@ const client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_SECRET,
 });
 
+const duration = ''
+const company = ''
+const position = ''
+const status = `After ${duration}, I have finally received my first job offer in tech! ` +
+                `I'm so excited to be starting at ${company} as a ${position}!`
+
 const params = {
-    status: 'This is a test tweet using the Twitter API.'
+    status: status
 }
 
-client.post('statuses/update', params, function(err, tweets, res) {
-    if(err) throw err;
-    console.log(tweets);
-    console.log(res);
-})
+if (status.length <= 240) {
+    client.post('statuses/update', params, function(err, tweets, res) {
+        if(err) throw err;
+        console.log(tweets);
+        console.log(res);
+    })
+} else {
+    console.log(`Character Count: ${status.length}`)
+}
